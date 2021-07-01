d91444e130bc48c41f7e543056935faeb49562f8
cz <starj1918@gmail.com>
2021-07-01 12:46:01 +0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import styles from '../styles/global.scss';
+import {StylesProvider} from '@material-ui/core';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+  return <StylesProvider injectFirst><Component {...pageProps} /></StylesProvider>;
 }
 
-export default MyApp
+export default MyApp;
